fd719f3e174c9f23b2396f9431b9977575c96a94
Bradley Willcott <bw.opensource@yahoo.com>
2025-08-24 20:02:21 +0800

    Idea: Working code.

---
+++ src/core/mod.rs
@@ -0,0 +1,49 @@
+//
+// File Name:    mod.rs
+// Directory:    src/core
+// Project Name: my_project
+//
+// Copyright (C) 2025 Bradley Willcott
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This library (crate) is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This library (crate) is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library (crate).  If not, see <https://www.gnu.org/licenses/>.
+//
+
+//!
+//! # Core mod
+//!
+
+pub mod control;
+
+use crate::*;
+
+const_logger!({
+    Logger::builder(module_path!())
+        .add_pconsole_handler()
+        //   ^^^^^^^^
+        .remove_file("test_logs/usage.log")
+        .add_file_handler("test_logs/usage.log")
+        // .set_level(Level::INFO)
+        .set_level(DEBUG_LEVEL)
+        //         ^^^^^^^^^^^
+        .build()
+});
+
+#[logger]
+pub fn do_it() {
+    entering!();
+    info!("This text came from the file `src/core/mod.rs`, just to let you know.");
+    exiting!();
+}
